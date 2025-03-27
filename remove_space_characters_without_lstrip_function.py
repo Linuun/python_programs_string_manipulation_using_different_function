913ad107b51abc179a9fca47d33219b5a7289ae4
Linuun <jlenonninolla31@gmail.com>
2025-03-27 23:30:33 +0800

    user input

--- remove_space_characters_without_lstrip_function.py
+++ remove_space_characters_without_lstrip_function.py
@@ -1,3 +1,4 @@
 # ask user to input with spaces in the beginning
+input_string = input("Enter any word/s (with spaces at the beginning): ")
 # use a different function to remove the spaces
 # print the output
