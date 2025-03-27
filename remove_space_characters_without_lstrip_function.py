d787f4f2c554395537361d1a6f975c2d307a43f0
Linuun <jlenonninolla31@gmail.com>
2025-03-27 23:41:45 +0800

    added some words in pseudocode

--- remove_space_characters_without_lstrip_function.py
+++ remove_space_characters_without_lstrip_function.py
@@ -1,4 +1,4 @@
-# ask user to input with spaces in the beginning
+# ask user to input a string with spaces in the beginning
 input_string = input("Enter any word/s (with spaces at the beginning): ")
 # use a different function to remove the spaces
 # print the output
