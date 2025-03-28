87e4464c4bd2e9093787a54e2c8975987e95b884
Linuun <jlenonninolla31@gmail.com>
2025-03-28 17:57:19 +0800

    user input

--- first_letter_capital_without_using_capitalize.py
+++ first_letter_capital_without_using_capitalize.py
@@ -1,2 +1,3 @@
 # ask user to input any word/s but the first letter is in lower case
+input_string = input("Enter any word/s (first letter in lower case): ")
 # print the output using upper and lower function
