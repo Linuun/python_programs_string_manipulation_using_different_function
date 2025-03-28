0030a5274f41a499769b6517a9d169860d5c2966
Linuun <jlenonninolla31@gmail.com>
2025-03-28 19:15:10 +0800

    print the output if the string starts with prefix

--- remove_characters_at_the_beginning_without_using_remove_prefix.py
+++ remove_characters_at_the_beginning_without_using_remove_prefix.py
@@ -2,3 +2,7 @@
 input_string = input("Enter any word/s: ")
 prefix = input("Enter a prefix: ")
 # print the output if the string starts with the prefix
+if input_string.startswith(prefix):
+    print(input_string[len(prefix):])
+else: 
+    print(input_string)
