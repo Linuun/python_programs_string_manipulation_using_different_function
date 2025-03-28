54a8ee483dce2f53ba7b8fb016a37d1a6e7878e0
Linuun <jlenonninolla31@gmail.com>
2025-03-28 17:45:26 +0800

    added pseudocode

---
+++ reverse_casing_without_using_swapcase.py
@@ -0,0 +1,6 @@
+# ask user to input words in improper casing
+# initialize a variable for storing characters
+# use for loop
+# check if the character is in upper case then add its lower case into the variable
+# check if the character is in lower case then add its upper case into the variable
+# print the output
