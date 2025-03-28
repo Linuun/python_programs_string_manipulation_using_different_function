487675fc5266e5724f2f89344c3abe6b4d4d93b5
Linuun <jlenonninolla31@gmail.com>
2025-03-28 18:20:17 +0800

    added pseudocode

---
+++ space_characters_at_the_beginning_and_end_without_using_center.py
@@ -0,0 +1,4 @@
+# ask user to input any word and the number of space characters they want
+# calculate the total number of space characters
+# calculate the number of space characters to add in left and right
+# print the output
