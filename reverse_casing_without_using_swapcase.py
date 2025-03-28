ba2cc3a49d6d8e004f452d08f9aba22544a6fe15
Linuun <jlenonninolla31@gmail.com>
2025-03-28 17:48:46 +0800

    check if the character is in uppercase then add its lowercase into the variable

--- reverse_casing_without_using_swapcase.py
+++ reverse_casing_without_using_swapcase.py
@@ -5,5 +5,7 @@ swapcase = ""
 # use for loop
 for letter in input_string:
 # check if the character is in upper case then add its lower case into the variable
+    if letter.isupper():
+        swapcase += letter.lower()
 # check if the character is in lower case then add its upper case into the variable
 # print the output
