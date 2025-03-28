0812bd7216ad53ee84641e3b95e6c654a6c8ff4d
Linuun <jlenonninolla31@gmail.com>
2025-03-28 18:50:07 +0800

    check if the length of suffix is greater than the string

--- check_if_the_end_part_matches_function_parameter_without_using_endswith.py
+++ check_if_the_end_part_matches_function_parameter_without_using_endswith.py
@@ -2,4 +2,6 @@
 input_string = input("Enter any word/s: ")
 suffix = input("Enter the suffix: ")
 # check if the length of suffix is greater than the string
+if len(suffix) > len(input_string):
+    print("False")
 # print the output by comparing the end of the string with the suffix
