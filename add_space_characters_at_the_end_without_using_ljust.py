5388852e621d602fc0b7108e1e3d6be505b50602
Linuun <jlenonninolla31@gmail.com>
2025-03-28 18:33:45 +0800

    user input

--- add_space_characters_at_the_end_without_using_ljust.py
+++ add_space_characters_at_the_end_without_using_ljust.py
@@ -1,3 +1,5 @@
 # ask user to input any words and the space characters they want
+input_string = input("Enter any word/s: ")
+space_characters = int(input("Enter the number of space characters you want: "))
 # calculate the number of space characters to add to the right
 # print the output
