42a0c6857ae8522b100825961e74107020164e68
Linuun <jlenonninolla31@gmail.com>
2025-03-28 18:05:42 +0800

    print the output using split, capitalize, and join function

--- first_letter_in_each_word_capital_without_using_title.py
+++ first_letter_in_each_word_capital_without_using_title.py
@@ -1,3 +1,4 @@
 # ask user to input words but first letter are in lower case
 input_string = input("Enter any words (first letter should be in lower case): ")
 # print the output by capitalizing every first letter using split, capitalize, and join fucntion
+print(" " .join(word.capitalize() for word in input_string.split()))
