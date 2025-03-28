6dca78398a1033ffef82e91d948bbe8639a9c037
Linuun <jlenonninolla31@gmail.com>
2025-03-28 19:03:36 +0800

    print the output by checking if input is equal to input with upper function

--- check_if_characters_in_upper_case_without_using_isupper.py
+++ check_if_characters_in_upper_case_without_using_isupper.py
@@ -1,3 +1,4 @@
 # ask user to input any word
 input_string = input("Enter any word: ")
 # print the output by checking if input is equal to input with upper function
+print(input_string == input_string.upper())
