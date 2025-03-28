71224d92e5ad7d3d5a082bbd22bb41374d02c247
Linuun <jlenonninolla31@gmail.com>
2025-03-28 18:45:05 +0800

    user input

--- check_if_the_end_part_matches_function_parameter_without_using_endswith.py
+++ check_if_the_end_part_matches_function_parameter_without_using_endswith.py
@@ -1,3 +1,5 @@
 # ask user to input any word/s and the suffix 
+input_string = input("Enter any word/s: ")
+suffix = input("Enter the suffix: ")
 # check if the length of suffix is greater than the string
 # print the output by comparing the end of the string with the suffix
