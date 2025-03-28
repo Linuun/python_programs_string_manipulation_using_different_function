b653b497c9cd78049531b7a5a42704968ddacdcf
Linuun <jlenonninolla31@gmail.com>
2025-03-28 18:21:26 +0800

    user input

--- space_characters_at_the_beginning_and_end_without_using_center.py
+++ space_characters_at_the_beginning_and_end_without_using_center.py
@@ -1,4 +1,6 @@
 # ask user to input any word and the number of space characters they want
+input_string = input("Enter any word/s: ")
+space_characters = int(input("Enter the number of space characters you want: "))
 # calculate the total number of space characters
 # calculate the number of space characters to add in left and right
 # print the output
