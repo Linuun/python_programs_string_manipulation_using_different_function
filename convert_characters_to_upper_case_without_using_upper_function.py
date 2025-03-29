185ddffbc224896cb39f388c7dd26d111c7c6b8d
Linuun <jlenonninolla31@gmail.com>
2025-03-29 22:12:56 +0800

    initialize a variable to store the letters

--- convert_characters_to_upper_case_without_using_upper_function.py
+++ convert_characters_to_upper_case_without_using_upper_function.py
@@ -1,6 +1,7 @@
 # ask user to input a string in lowercase
 input_string = input("Enter any word/s (in lowercase): ")
 # initialize a variable to store the letters
+result = ""
 # use for loop
 # check if the letter is in lowercase
 # add the capitalized letter to the variable
