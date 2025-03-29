a62a0cb00d9810cbbe290889c4fb7b078e0c548d
Linuun <jlenonninolla31@gmail.com>
2025-03-29 22:04:17 +0800

    print the output if the string ends with the suffix

--- remove_characters_at_the_end_without_using_remove_suffix.py
+++ remove_characters_at_the_end_without_using_remove_suffix.py
@@ -2,3 +2,7 @@
 input_string = input("Enter any word/s: ")
 suffix = input("Enter a suffix: ")
 # print the output if the string ends with the suffix
+if input_string.endswith(suffix):
+    print(input_string[:-len(suffix)])
+else:
+    print(input_string)
