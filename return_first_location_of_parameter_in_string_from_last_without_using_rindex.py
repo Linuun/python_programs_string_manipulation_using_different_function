3353065afe493694212542d684716bf3c938abe6
Linuun <jlenonninolla31@gmail.com>
2025-03-29 22:44:14 +0800

    print Error if the string was not found in the input

--- return_first_location_of_parameter_in_string_from_last_without_using_rindex.py
+++ return_first_location_of_parameter_in_string_from_last_without_using_rindex.py
@@ -2,4 +2,6 @@
 input_string = input("Enter any word/s: ")
 sub_string = input("Enter the string you want to locate: ")
 # print Error if the string was not found in the input
+if sub_string not in input_string:
+    print("Error: string not found")
 # print the output using rfind function
