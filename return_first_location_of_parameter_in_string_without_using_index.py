4019b341c4b0d413ccaaf22f93178243c58904e5
Linuun <jlenonninolla31@gmail.com>
2025-03-29 22:35:16 +0800

    user input

--- return_first_location_of_parameter_in_string_without_using_index.py
+++ return_first_location_of_parameter_in_string_without_using_index.py
@@ -1,3 +1,5 @@
 # ask user to input any word/s and the string they want to locate
+input_string = input("Enter any word/s: ")
+sub_string = input("Enter the string you want to locate: ")
 # print Error if the string was not found in the input
 # print the output using find function
