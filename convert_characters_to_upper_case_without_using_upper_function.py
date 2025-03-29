5e963d5e3a640b2297a9b52ec50d325441bccc09
Linuun <jlenonninolla31@gmail.com>
2025-03-29 22:11:51 +0800

    user input

--- convert_characters_to_upper_case_without_using_upper_function.py
+++ convert_characters_to_upper_case_without_using_upper_function.py
@@ -1,4 +1,5 @@
 # ask user to input a string in lowercase
+input_string = input("Enter any word/s (in lowercase): ")
 # initialize a variable to store the letters
 # use for loop
 # check if the letter is in lowercase
