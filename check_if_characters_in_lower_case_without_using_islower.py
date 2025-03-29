68cc44ef5141845e9754c81de1f31fb5cd574f1b
Linuun <jlenonninolla31@gmail.com>
2025-03-29 22:23:57 +0800

    print the output by checking if input is equal to input with lower function

--- check_if_characters_in_lower_case_without_using_islower.py
+++ check_if_characters_in_lower_case_without_using_islower.py
@@ -1,3 +1,4 @@
 # ask user to input any word
 input_string = input("Enter any word: ")
 # print the output by checking if input is equal to input with lower function
+print(input_string == input_string.lower())
