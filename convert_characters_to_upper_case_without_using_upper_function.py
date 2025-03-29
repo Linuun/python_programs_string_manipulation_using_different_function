9585ee4b5cf0b2b4ba6e77171f558cbbe65dc60e
Linuun <jlenonninolla31@gmail.com>
2025-03-29 22:10:35 +0800

    added pseudocode

---
+++ convert_characters_to_upper_case_without_using_upper_function.py
@@ -0,0 +1,6 @@
+# ask user to input a string in lowercase
+# initialize a variable to store the letters
+# use for loop
+# check if the letter is in lowercase
+# add the capitalized letter to the variable
+# print the output
