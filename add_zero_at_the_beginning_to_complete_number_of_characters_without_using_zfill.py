78c7587138da0c05d8014a2514f9e64f278d0482
Linuun <jlenonninolla31@gmail.com>
2025-03-30 21:55:37 +0800

    user input

--- add_zero_at_the_beginning_to_complete_number_of_characters_without_using_zfill.py
+++ add_zero_at_the_beginning_to_complete_number_of_characters_without_using_zfill.py
@@ -1,2 +1,4 @@
 # ask the user to input any word and the zeros needed to fill
+input_string = input("Enter any word: ")
+number = int(input("Enter the zeros to fill: "))
 # print the output using f string function
