69e4691b910eb84e2b4c1e0ab2e95782271f807d
Linuun <jlenonninolla31@gmail.com>
2025-03-30 22:23:26 +0800

    added pseudocode

---
+++ return_how_many_appearance_parameter_in_the_string_without_using_count.py
@@ -0,0 +1,7 @@
+# ask user to input any word/s and the character/s they want to count
+# initialize a variable for count and start
+# use while loop
+# find the position of the character starting from "start" index
+# check if the character is found in the string, add 1 to the count and update the start position
+# break the loop
+# print the output
