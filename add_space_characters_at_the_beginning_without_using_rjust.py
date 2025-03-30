5f16713b7379f641e49ff161eb8585794ac78959
Linuun <jlenonninolla31@gmail.com>
2025-03-30 21:37:10 +0800

    user input

--- add_space_characters_at_the_beginning_without_using_rjust.py
+++ add_space_characters_at_the_beginning_without_using_rjust.py
@@ -1,3 +1,5 @@
 # ask user to input any words and the space they want
+input_string = input("Enter any word/s: ")
+space_characters = int(input("Enter the number of space characters you want: "))
 # calculate the number of space characters to add to the left
 # print the output
