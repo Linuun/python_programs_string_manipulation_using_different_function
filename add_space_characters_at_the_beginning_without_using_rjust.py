28a8d6032f0bcd851b7f8474466cfcef8f84b908
Linuun <jlenonninolla31@gmail.com>
2025-03-30 21:39:03 +0800

    calculate the number of space characters to add to the left

--- add_space_characters_at_the_beginning_without_using_rjust.py
+++ add_space_characters_at_the_beginning_without_using_rjust.py
@@ -2,4 +2,7 @@
 input_string = input("Enter any word/s: ")
 space_characters = int(input("Enter the number of space characters you want: "))
 # calculate the number of space characters to add to the left
+total_spaces = space_characters - len(input_string)
+if total_spaces <= 0:
+    print(input_string)
 # print the output
