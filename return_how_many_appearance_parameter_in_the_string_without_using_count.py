05b12078cef341abfa60a8006dd0bb22ef92457f
Linuun <jlenonninolla31@gmail.com>
2025-03-30 22:25:20 +0800

    initialize a variable for count and start

--- return_how_many_appearance_parameter_in_the_string_without_using_count.py
+++ return_how_many_appearance_parameter_in_the_string_without_using_count.py
@@ -2,6 +2,8 @@
 input_string = input("Enter any word/s: ")
 sub_string = input("Enter the character you want to count the number of appearance: ")
 # initialize a variable for count and start
+count = 0
+start = 0
 # use while loop
 # find the position of the character starting from "start" index
 # check if the character is found in the string, add 1 to the count and update the start position
