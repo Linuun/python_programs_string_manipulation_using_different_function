30ef439010b31af09876b97df4a82b9e754acea1
Linuun <jlenonninolla31@gmail.com>
2025-03-30 22:06:41 +0800

    check if the prefix matches the beginning of the string and print the output

--- check_if_the_beginning_part_matches_function_parameter_without_using_startswith.py
+++ check_if_the_beginning_part_matches_function_parameter_without_using_startswith.py
@@ -2,3 +2,7 @@
 input_string = input("Enter any word/s: ")
 prefix = input("Enter the prefix: ")
 # check if the prefix matches the beginning of the string and print the output
+if input_string[:len(prefix)] == prefix:
+    print("True")
+else:
+    print("False")
