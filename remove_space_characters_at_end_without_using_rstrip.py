8aaaeb5110b589f0b48b525627dd36d8bf97f9fd
Linuun <jlenonninolla31@gmail.com>
2025-03-30 22:42:40 +0800

    use while loop and remove the spaces at the end of the string

--- remove_space_characters_at_end_without_using_rstrip.py
+++ remove_space_characters_at_end_without_using_rstrip.py
@@ -1,4 +1,6 @@
 # ask user to input any word/s
 input_string = input("Enter any word/s: ")
 # use while loop and remove the spaces at the end of the string
+while input_string and input_string[-1] == " ":
+    input_string = input_string[:-1]
 # print the output
